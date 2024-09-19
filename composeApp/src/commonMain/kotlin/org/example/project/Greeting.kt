0d6dce7256eed79c282b1f3310d58201951bd8e1
Manuel Paulo Afonso <manuelpauloafonso29@gmail.com>
2024-09-19 11:31:33 +0100

    first commit

---
+++ composeApp/src/commonMain/kotlin/org/example/project/Greeting.kt
@@ -0,0 +1,13 @@
+package org.example.project
+import kotlin.random.Random
+
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello!"
+
+        return "$firstWord Guess what this is! > ${platform.name.reversed()}!"
+    }
+}
